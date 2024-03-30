cea695b55981f28c432dc07aec46aa74797a5bc1
Sufiane <su.souissi@gmail.com>
2024-03-30 17:07:24 +0100

    feat(api): add GET translation endpoint

---
+++ api/routes/translation/schemas/index.ts
@@ -0,0 +1,5 @@
+import { z } from 'zod';
+
+export const paramsSchema = z.object({
+    jobId: z.coerce.number(),
+})
---
+++ services/deepl.ts
@@ -0,0 +1,14 @@
+import * as deepl from 'deepl-node';
+
+const DEEPL_API_KEY = process.env.DEEPL_API_KEY
+
+export const translate = async (text: string): Promise<string> => {
+    if (!DEEPL_API_KEY) {
+        throw new Error('Missing deepl api key.')
+    }
+    const translator = new deepl.Translator(DEEPL_API_KEY);
+
+    const translationResult = await translator.translateText(text, null, 'en-GB')
+
+    return translationResult.text
+}
